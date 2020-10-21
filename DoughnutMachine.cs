1e6449d4513a1b675ed4463e3e0f821f36630b81
CRISTINA NEAG <neag.cristina165320@student.ubbcluj.ro>
2020-10-21 11:29:26 +0300

    Adaugarea clasei DoughnutMachine

---
+++ DoughnutMachine.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Neag_Cristina_Lab2
+{
+    public enum DoughnutType
+    {
+        Glazed,
+        Sugar,
+        Lemon,
+        Chocolate,
+        Vanilla
+    }
+
+    class Doughnut
+    {
+        private DoughnutType mFlavor;
+        public DoughnutType Flavor
+        {
+            get
+            {
+                return mFlavor;
+            }
+            set
+            {
+                mFlavor = value;
+            }
+        }
+        private float mPrice = .50F;
+        public float Price
+        {
+            get
+            {
+                return mPrice;
+            }
+            set
+            {
+                mPrice = value;
+            }
+        }
+        private readonly DateTime mTimeOfCreation;
+        public DateTime TimeOfCreation
+        {
+            get
+            {
+                return mTimeOfCreation;
+            }
+        }
+        public Doughnut(DoughnutType aFlavor) // constructor
+        {
+            mTimeOfCreation = DateTime.Now;
+            mFlavor = aFlavor;
+        }
+    }
+
+    private DoughnutType mFlavor;
+    public DoughnutType Flavor
+    {
+        get
+        {
+            return mFlavor;
+        }
+        set
+        {
+            mFlavor = value;
+        }
+    class DoughnutMachine
+    {
+    }
+}
