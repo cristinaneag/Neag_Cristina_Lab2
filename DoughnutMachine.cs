f984d5ce56914af0e65f4151f78a5781ef4e8dde
CRISTINA NEAG <neag.cristina165320@student.ubbcluj.ro>
2020-10-21 12:04:07 +0300

    Am adaugat noi metode si event-handlere

--- DoughnutMachine.cs
+++ DoughnutMachine.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
+using System.Windows.Threading;
 
 namespace Neag_Cristina_Lab2
 {
@@ -53,19 +55,80 @@ namespace Neag_Cristina_Lab2
             mFlavor = aFlavor;
         }
     }
+    class DoughnutMachine : Component
+    {
+        public DoughnutMachine()
+        {
+            InitializeComponent();
+        }
 
-    private DoughnutType mFlavor;
-    public DoughnutType Flavor
-    {
-        get
+        private DoughnutType mFlavor;
+        public DoughnutType Flavor
         {
-            return mFlavor;
+            get
+            {
+                return mFlavor;
+            }
+            set
+            {
+                mFlavor = value;
+            }
         }
-        set
+        private System.Collections.ArrayList mDoughnuts = new System.Collections.ArrayList();
+        public Doughnut this[int Index]
         {
-            mFlavor = value;
+            get
+            {
+                return (Doughnut)mDoughnuts[Index];
+            }
+            set
+            {
+                mDoughnuts[Index] = value;
+            }
         }
-    class DoughnutMachine
-    {
+        public delegate void DoughnutCompleteDelegate();
+        public event DoughnutCompleteDelegate DoughnutComplete;
+
+        DispatcherTimer doughnutTimer;
+
+        private void InitializeComponent()
+        {
+            this.doughnutTimer = new DispatcherTimer();
+            this.doughnutTimer.Tick += new System.EventHandler(this.doughnutTimer_Tick);
+        }
+        private void doughnutTimer_Tick(object sender, EventArgs e)
+        {
+            Doughnut aDoughnut = new Doughnut(this.Flavor);
+            mDoughnuts.Add(aDoughnut);
+            DoughnutComplete();
+        }
+        public bool Enabled
+        {
+            set
+            {
+                doughnutTimer.IsEnabled = value;
+            }
+        }
+        public int Interval
+        {
+            set
+            {
+                doughnutTimer.Interval = new TimeSpan(0, 0, value);
+            }
+        }
+
+        public void MakeDoughnuts(DoughnutType dFlavor)
+        {
+            Flavor = dFlavor;
+            switch (Flavor)
+            {
+                case DoughnutType.Glazed: Interval = 3; break;
+                case DoughnutType.Sugar: Interval = 2; break;
+                case DoughnutType.Lemon: Interval = 5; break;
+                case DoughnutType.Chocolate: Interval = 7; break;
+                case DoughnutType.Vanilla: Interval = 4; break;
+            }
+            doughnutTimer.Start();
+        }
     }
 }
